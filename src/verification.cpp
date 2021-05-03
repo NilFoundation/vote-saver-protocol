4788ecf4a1b5861781962d47547d1b62bb71710f
Mikhail Komarov <nemo@nil.foundation>
2021-05-04 01:38:11 +0300

    Examples updated.

--- src/verification.cpp
+++ src/verification.cpp
@@ -1,29 +1,38 @@
+#include "verification.hpp"
+
 #include <tvm/contract.hpp>
+#include <tvm/smart_switcher.hpp>
 #include <tvm/contract_handle.hpp>
 #include <tvm/default_support_functions.hpp>
-#include <tvm/smart_switcher.hpp>
-
-#include "verification.hpp"
 
 using namespace tvm;
 using namespace schema;
 
 static constexpr unsigned ROOT_TIMESTAMP_DELAY = 100;
 
-class RootTokenContract final : public smart_interface<IRootTokenContract>, public DRootTokenContract {
+class verificator final : public smart_interface<iverificator>, public dverificator {
 public:
     using root_replay_protection_t = replay_attack_protection::timestamp<ROOT_TIMESTAMP_DELAY>;
 
     __always_inline void constructor(bytes proof_msg_bytes) {
         proof_msg_ = proof_msg_bytes.cl_;
     }
+
     __always_inline bool_t verify() {
         return (__builtin_tvm_vergrth16(proof_msg_) > 0) ? bool_t(1) : bool_t(0);
     }
+
+    // default processing of unknown messages
+    __always_inline static int _fallback(cell msg, slice msg_body) {
+        return 0;
+    }
+
+    // =============== Support functions ==================
+    DEFAULT_SUPPORT_FUNCTIONS(iverificator, root_replay_protection_t)
 };
 
-DEFINE_JSON_ABI(IRootTokenContract, DRootTokenContract, ERootTokenContract);
+DEFINE_JSON_ABI(iverificator, dverificator, everificator);
 
 // ----------------------------- Main entry functions ---------------------- //
-DEFAULT_MAIN_ENTRY_FUNCTIONS(RootTokenContract, IRootTokenContract, DRootTokenContract, ROOT_TIMESTAMP_DELAY)
+DEFAULT_MAIN_ENTRY_FUNCTIONS(verificator, iverificator, dverificator, ROOT_TIMESTAMP_DELAY)
 
--- src/verification.hpp
+++ src/verification.hpp
@@ -4,19 +4,19 @@ namespace tvm {
     namespace schema {
 
         // ===== Root Token Contract (Non-fungible) ===== //
-        __interface IRootTokenContract {
+        __interface iverificator {
+
             // expected offchain constructor execution
             __attribute__((internal, external, dyn_chain_parse)) void constructor(bytes proof_msg_bytes) = 11;
 
             __attribute__((getter)) bool_t verify() = 12;
         };
 
-        struct DRootTokenContract {
+        struct dverificator {
             cell proof_msg_;
         };
 
-        struct ERootTokenContract { };
-
+        struct everificator { };
     }    // namespace schema
 }    // namespace tvm
 

c5cbf246393add34f40a5742be381017927290fa
noaNoamDev <noam.yem@gmail.com>
2022-04-18 21:43:08 +0300

    add first 3 phases

---
+++ share/wasm/test.js
@@ -0,0 +1,20 @@
+const wrapper = require("./wrapper.js");
+
+function test() {
+    tree_depth = 2;
+    eid = 1;
+    num_participants = 1 << tree_depth;
+    keypairs = [];
+    public_keys = [];
+    for(var i =0; i < num_participants; ++i) {
+        keypair = wrapper.generate_voter_keypair();
+        keypairs.push(keypair);
+        public_keys.push(keypair.public_key);
+    }
+    election = wrapper.init_election(tree_depth, public_keys);
+    vote_data = wrapper.generate_vote(tree_depth, 0, 1, public_keys,
+        election.rt, election.eid, keypairs[0].secret_key,
+        election.public_key, election.r1cs_proving_key, election.r1cs_verification_key);
+    console.log(vote_data)
+}
+setTimeout(test, 300);
